3cbb15d86bc5c0ba9410f6c8f15cf620aa69f65c
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2026-06-27 21:49:54 +0300

    feat: configure services and add controller mapping in Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Progredi.DataAccess;
+using Progredi.Interfaces;
+using Progredi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -7,12 +9,20 @@ var configuration = builder.Configuration;
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(
     options => {
         options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
     });
 
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
 
 // app.UseSwagger();
 // app.UseSwaggerUI();
