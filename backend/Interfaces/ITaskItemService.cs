cfa0eeee66a4e906d3a741051802145ad3074dcd
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2026-07-01 17:38:26 +0300

    refactor: remove async modifier from TaskItemService interface methods

--- backend/Interfaces/ITaskItemService.cs
+++ backend/Interfaces/ITaskItemService.cs
@@ -5,7 +5,7 @@ namespace Progredi.Interfaces;
 
 public interface ITaskItemService
 {
-    public async Task<TaskItemResponseDto> CreateAsync(Guid userId, CreateTaskItemDto dto);
-    public async Task<TaskItemResponseDto> UpdateAsync(Guid userId, Guid taskItemId, UpdateTaskItemDto dto);
-    public async Task<TaskItemResponseDto> DeleteAsync(Guid userId, Guid taskItemId);
+    public Task<TaskItemResponseDto> CreateAsync(Guid userId, CreateTaskItemDto dto);
+    public Task<TaskItemResponseDto> UpdateAsync(Guid userId, Guid taskItemId, UpdateTaskItemDto dto);
+    public Task<TaskItemResponseDto> DeleteAsync(Guid userId, Guid taskItemId);
 }
